021c1160b1b37c8c5b565379f3ca2e90447fa7a4
Jonathan QUEREL <jonathan.querel@gmail.com>
2023-08-21 16:34:34 +0200

    Finalisation projet

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react"; /* importe la librairie REACT*/
+import ReactDOM from "react-dom/client"; /* importe la librairie ReactDOM*/
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom"; /* Importe le RouterReact pour faire les liens / redirection*/
 import Header from "./components/Header/header";
 import Home from "./pages/Home/Home.js";
 import APropos from "./pages/APropos/APropos.js";
@@ -8,9 +12,12 @@ import Error from "./pages/Error/Error";
 import Footer from "./components/Footer/footer";
 import FicheLogement from "./pages/FicheLogement/FicheLogement.js";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(
+  document.getElementById("root")
+); /* Injecte tout le code JS dans la balise ayant l'id "root"*/
 
 root.render(
+  /* Fonction de base de react qui permet de construire les éléments React sur la page*/
   <React.StrictMode>
     <Router>
       <Header logo="../assets/logo_header_kasa.png" />
